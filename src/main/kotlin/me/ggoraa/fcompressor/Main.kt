3e0ce8db58fed3c447684f12adac93fe31106971
Yegor Yakovenko <ggoraa1029@gmail.com>
2021-03-01 17:59:33 +0200

    Added the main logic

--- src/main/kotlin/me/ggoraa/fcompressor/Main.kt
+++ src/main/kotlin/me/ggoraa/fcompressor/Main.kt
@@ -1,33 +1,84 @@
+@file:Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+
+package me.ggoraa.fcompressor
+
 import com.xenomachina.argparser.ArgParser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import me.ggoraa.fcompressor.args.ProgramArgs
+import net.bramp.ffmpeg.FFmpeg
+import net.bramp.ffmpeg.FFmpegExecutor
+import net.bramp.ffmpeg.FFprobe
+import net.bramp.ffmpeg.builder.FFmpegBuilder
 import java.io.File
 
-fun main(args: Array<String>) {
-    // Splash
-    println("    ____________                                                    \n" +
-            "   / ____/ ____/___  ____ ___  ____  ________  ______________  _____\n" +
-            "  / /_  / /   / __ \\/ __ `__ \\/ __ \\/ ___/ _ \\/ ___/ ___/ __ \\/ ___/\n" +
-            " / __/ / /___/ /_/ / / / / / / /_/ / /  /  __(__  |__  ) /_/ / /    \n" +
-            "/_/    \\____/\\____/_/ /_/ /_/ .___/_/   \\___/____/____/\\____/_/     \n" +
-            "                           /_/                                      \n")
+suspend fun main(args: Array<String>) = coroutineScope {
+        // Splash
+        println("    ____________                                                    \n" +
+                "   / ____/ ____/___  ____ ___  ____  ________  ______________  _____\n" +
+                "  / /_  / /   / __ \\/ __ `__ \\/ __ \\/ ___/ _ \\/ ___/ ___/ __ \\/ ___/\n" +
+                " / __/ / /___/ /_/ / / / / / / /_/ / /  /  __(__  |__  ) /_/ / /    \n" +
+                "/_/    \\____/\\____/_/ /_/ /_/ .___/_/   \\___/____/____/\\____/_/     \n" +
+                "                           /_/                                      \n")
 
-    println("A convenient tool for compressing a lot of videos at the same time with no effort\n\n")
-    ArgParser(args).parseInto(::ProgramArgs).run {
-        println("Input: $inputDir")
-        println("Output: $outputDir")
-        println("Getting all video files...")
+        println("A convenient tool for compressing a lot of videos at the same time with no effort\n\n")
 
-        val inputFilesRegex = """mp4$|avi$|mkv$""".toRegex() // Regex for filtering out videos
+        val ffmpeg = FFmpeg("ffmpeg")
+        val ffprobe = FFprobe("ffprobe")
 
-        val inputFilesFiltered = mutableListOf<String>()
-        val filesInInput = File(inputDir).list()
+        val inputFilesFiltered: MutableList<String>
+        val ffmpegCodec: String
+        val ffmpegCrf: Int
+        val ffmpegInputDir: String
+        val ffmpegOutputDir: String
 
-        for (i in filesInInput.indices){
-            if(inputFilesRegex.containsMatchIn(filesInInput[i])) {
-                inputFilesFiltered.add(filesInInput[i])
+        ArgParser(args).parseInto(::ProgramArgs).run {
+            println("Input: $inputDir")
+            println("Output: $outputDir")
+            println("Checking input and output directories...")
+
+            // Check for if those directories actually exist
+            if(!File(inputDir).isDirectory) {
+                throw IllegalArgumentException("Input directory does not exist")
+            }
+            if(!File(outputDir).isDirectory) {
+                throw IllegalArgumentException("Output directory does not exist")
+            }
+
+            println("Getting all video files...")
+
+            val inputFilesRegex = """mp4$|avi$|mkv$""".toRegex() // Regex for filtering out videos
+
+            inputFilesFiltered = mutableListOf<String>()
+            val filesInInput = File(inputDir).list()
+
+            for (i in filesInInput.indices) {
+                if (inputFilesRegex.containsMatchIn(filesInInput[i])) {
+                    inputFilesFiltered.add(filesInInput[i])
+                }
+            }
+            ffmpegCrf = crf
+            ffmpegCodec = codec
+            ffmpegInputDir = inputDir
+            ffmpegOutputDir = outputDir
+        }
+        println("Starting the compression process...")
+
+        for (i in inputFilesFiltered.indices) {
+            val builder = FFmpegBuilder()
+                .setInput("$ffmpegInputDir/${inputFilesFiltered[i]}")
+                .overrideOutputFiles(false)
+                .addOutput("$ffmpegOutputDir/${inputFilesFiltered[i]}")
+                .setVideoCodec(ffmpegCodec)
+                .setConstantRateFactor(ffmpegCrf.toDouble())
+                .done()
+
+            val executor = FFmpegExecutor(ffmpeg, ffprobe)
+            launch(Dispatchers.IO) {
+                executor.createJob(builder).run()
             }
         }
-        println(inputFilesFiltered)
-
+        println("FCompressor is now compressing the videos, the program will automatically exit on finish")
     }
-}
